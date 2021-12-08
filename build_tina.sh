e404903b352334db378cf1f6cf8c4d0c870a97fa
Pietro Bonfa <bonfus@gmail.com>
2021-12-08 15:38:13 +0100

    Wheels generation

--- build_tina.sh
+++ build_tina.sh
@@ -8,5 +8,8 @@ wget https://github.com/rabauke/trng4/archive/refs/tags/v$VER.tar.gz
 tar xzf v4.24.tar.gz
 cd trng4-$VER
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SCRIPT_PATH/tina  CXXFLAGS='-fPIC' ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SCRIPT_PATH/tina -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" ..
 make && make install
+cd ../..
+rm -r trng4-$VER
+
